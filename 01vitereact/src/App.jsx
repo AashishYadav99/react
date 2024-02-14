400933cbe188def84b1fe5a906764675f80e993f
Vimlesh <vimlesh@healtharc.io>
2024-02-15 00:18:23 +0530

    Basic of react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (<div>
+       <h1>Hi guys its me Ashish yadav THIS IS MY FIRST REACT CODE</h1>
+       <h3>if you wanna be profissional react you should do practice and practice </h3>
+       <img src="c:\Users\ashis\OneDrive\Pictures\Saved Pictures\bbdu logo.jpeg" alt="image not found" />
+       </div>)
+}
+
+export default App
---
+++ 01vitereact/src/photos.jsx
@@ -0,0 +1,14 @@
+function Photo() {
+
+    return (<div>
+        <form action="">
+        <label htmlFor="">Name</label>
+        <input type="text" />
+        <label htmlFor="">password</label>
+        <input type="password" />
+
+        </form>
+         </div>)
+  }
+  
+  export default Photo
